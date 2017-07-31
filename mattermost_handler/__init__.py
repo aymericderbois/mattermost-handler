668931f94877ec200f5456adaea1e1f55c8d0082
Derbois Aymeric <aymeric@derbois.com>
2017-07-31 12:47:49 +0200

    Change behavior, and name

--- mattermost_handler/__init__.py
+++ mattermost_handler/__init__.py
@@ -3,14 +3,11 @@
 import logging
 import requests
 
-URL = None
 
-
-class MattermostHandler(logging.Handler):
-    def __init__(self):
-        if URL is None:
-            raise Exception('MattermostHandler: you need to define mattermost_handler.URL with your income url')
-        super(MattermostHandler, self).__init__()
+class MattermostIncomeWebhookHandler(logging.Handler):
+    def __init__(self, url):
+        super(MattermostIncomeWebhookHandler, self).__init__()
+        self.url = url
 
     def emit(self, record):
-        requests.post(URL, json={"text": self.format(record)})
+        requests.post(self.url, json={"text": self.format(record)})
