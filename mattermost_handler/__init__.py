4c4d41540c16310ea021d012a26f1b033e0b0be6
Aymeric Derbois <aymeric@derbois.com>
2018-12-06 15:37:31 +0100

    If url is not, write a warning, and dont post

--- mattermost_handler/__init__.py
+++ mattermost_handler/__init__.py
@@ -5,11 +5,17 @@ name = "mattermost_handler"
 import logging
 import requests
 
+logger = logging.getLogger(__name__)
+
 
 class MattermostIncomeWebhookHandler(logging.Handler):
     def __init__(self, url):
         super(MattermostIncomeWebhookHandler, self).__init__()
         self.url = url
 
+        if self.url is None:
+            logger.warning("Mattermost webhook url cannot be None")
+
     def emit(self, record):
-        requests.post(self.url, json={"text": self.format(record)})
+        if self.url is not None:
+            requests.post(self.url, json={"text": self.format(record)})
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='mattermost-handler',
-    version='1.0.2',
+    version='1.0.3',
     description='A basic log handler for mattermost income url',
     author='Aymeric Derbois',
     author_email='aymeric@derbois.com',
